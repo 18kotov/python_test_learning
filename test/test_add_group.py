345cb3c6f84bac0ceeb3927afb5ea5668e2485d0
18kotov <twozerocool@gmail.com>
2017-12-12 15:43:11 +0300

    Добавлен помошник GroupHelper

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -15,18 +15,18 @@ def app(request):
 def test_add_group(app):
     app.open_start_page()
     app.session.login(username="admin", password="secret")
-    app.open_groups_page()
-    app.create_group(Group(group_name="ddfg", header="4fff", footer="fbhj"))
-    app.return_to_groups_page()
+    app.group.open_groups_page()
+    app.group.create(Group(group_name="ddfg", header="4fff", footer="fbhj"))
+    app.group.return_to_groups_page()
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.open_start_page()
     app.session.login( username="admin", password="secret")
-    app.open_groups_page()
-    app.create_group( Group(group_name="", header="", footer=""))
-    app.return_to_groups_page()
+    app.group.open_groups_page()
+    app.group.create(Group(group_name="", header="", footer=""))
+    app.group.return_to_groups_page()
     app.session.logout()
 
 
