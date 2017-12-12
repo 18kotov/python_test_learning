a2c7a74ca67c90af0addf2e4169a8ebc2c4d9191
18kotov <twozerocool@gmail.com>
2017-12-12 15:51:56 +0300

    Добавлен помошник ContactHelper

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import  ContactHelper
 
 
 
@@ -12,10 +13,7 @@ class Application():
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
-
-
-
-
+        self.contact = ContactHelper(self)
 
 
 
@@ -23,47 +21,6 @@ class Application():
         wd = self.wd
         wd.get("http://localhost/addressbook/")
 
-
-    def go_home_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("home").click()
-
-    def add_new_contact(self, contact):
-        #fill_contact_info
-        wd = self.wd
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        #clic
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def open_add_new_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -17,9 +17,9 @@ def app(request):
 def test_test_add_contact(app):
     app.open_start_page()
     app.session.login(username="admin", password="secret")
-    app.open_add_new_page()
-    app.add_new_contact(Contact("ssdr", "rr", "rrr", "ghjk", "hjj", "rrt", "tttt", "456", "rrtt@rrt.ty"))
-    app.go_home_page()
+    app.contact.open_add_new_page()
+    app.contact.add_new(Contact("ssdr", "rr", "rrr", "ghjk", "hjj", "rrt", "tttt", "456", "rrtt@rrt.ty"))
+    app.contact.go_home_page()
     app.session.logout()
 
 
