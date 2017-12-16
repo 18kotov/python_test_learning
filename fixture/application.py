40e689ffcc40be93314104cc3b7aebf588348fad
18kotov <twozerocool@gmail.com>
2017-12-16 13:35:49 +0300

    Добавлен тест редактирования группы

--- fixture/application.py
+++ fixture/application.py
@@ -6,6 +6,7 @@ from fixture.contact import  ContactHelper
 
 
 
+
 class Application():
 
     def __init__(self):
@@ -14,6 +15,7 @@ class Application():
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+
 
 
 
