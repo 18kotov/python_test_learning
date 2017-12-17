18303379354b151ed0ebfaf16ea7b6c170fc6304
18kotov <twozerocool@gmail.com>
2017-12-17 13:22:23 +0300

    Добавлен тест modify

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -15,13 +15,13 @@ def test_add_group(app):
     app.session.logout()
 
 
-def test_add_empty_group(app):
-    app.open_start_page()
-    app.session.login( username="admin", password="secret")
-    app.group.open_groups_page()
-    app.group.create(Group(group_name="", header="", footer=""))
-    app.group.return_to_groups_page()
-    app.session.logout()
+# def test_add_empty_group(app):
+#     app.open_start_page()
+#     app.session.login( username="admin", password="secret")
+#     app.group.open_groups_page()
+#     app.group.create(Group(group_name="", header="", footer=""))
+#     app.group.return_to_groups_page()
+#     app.session.logout()
 
 
 
