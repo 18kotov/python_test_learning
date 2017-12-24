f5bee4ab0d9d529d75b0edc7847bb8248127a883
18kotov <twozerocool@gmail.com>
2017-12-24 15:09:02 +0300

    Добавлены проверки сравнением списков в методы добавления групп

--- model/group.py
+++ model/group.py
@@ -1,3 +1,5 @@
+from sys import maxsize
+
 
 class Group:
     def __init__(self, group_name=None, header=None, footer=None, id=None):
@@ -11,4 +13,11 @@ class Group:
         return "%s:%s" % (self.id, self.group_name)
 
     def __eq__(self, other):
-        return self.id==other.id and self.group_name==other.group_name
+        return (self.id is None or other.id is None or self.id==other.id) and self.group_name==other.group_name
+
+
+    def id_or_max(gr):
+        if gr.id:
+            return int(gr.id)
+        else:
+            return maxsize
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,14 +5,18 @@ from model.group import Group
 
 
 
+
     
 def test_add_group(app):
     app.group.open_groups_page()
     old_groups=app.group.get_group_list()
-    app.group.create(Group(group_name="ddfg", header="4fff", footer="fbhj"))
+    group=Group(group_name="ddfg", header="4fff", footer="fbhj")
+    app.group.create(group)
     app.group.return_to_groups_page()
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
@@ -20,10 +24,13 @@ def test_add_group(app):
 def test_add_empty_group(app):
     app.group.open_groups_page()
     old_groups = app.group.get_group_list()
-    app.group.create(Group(group_name="", header="", footer=""))
+    group = Group(group_name="", header="", footer="")
+    app.group.create(group)
     app.group.return_to_groups_page()
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
