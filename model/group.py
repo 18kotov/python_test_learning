0765c77bfea579d359db2311ca84bd347b23123e
18kotov <twozerocool@gmail.com>
2017-12-24 14:45:34 +0300

    Добавлен метод сравнения списков. Добавлен метод отображения.

--- model/group.py
+++ model/group.py
@@ -5,3 +5,10 @@ class Group:
         self.header=header
         self.footer=footer
         self.id=id
+
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.group_name)
+
+    def __eq__(self, other):
+        return self.id==other.id and self.group_name==other.group_name
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,7 +3,10 @@ from model.group import Group
 
 def test_modify_group_name(app):
     app.group.open_groups_page()
+    old_groups = app.group.get_group_list()
     if app.group.count()==0:
         app.group.create(Group(group_name='test'))
     app.group.modify_first_group(Group(group_name='New group'))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups)  == len(new_groups)
     app.group.return_to_groups_page()
