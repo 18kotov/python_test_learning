a9b60449d267154ad4aa4886703d83eae08f1628
18kotov <twozerocool@gmail.com>
2017-12-24 15:16:48 +0300

    Добавлены проверки сравнением списков в метод модификации групп

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,9 +4,13 @@ from model.group import Group
 def test_modify_group_name(app):
     app.group.open_groups_page()
     old_groups = app.group.get_group_list()
+    group=Group(group_name='New group')
+    group.id=old_groups[0].id
     if app.group.count()==0:
         app.group.create(Group(group_name='test'))
-    app.group.modify_first_group(Group(group_name='New group'))
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups)  == len(new_groups)
+    old_groups[0]=group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     app.group.return_to_groups_page()
