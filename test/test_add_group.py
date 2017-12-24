d6a4df7d51ec4b551819d4f027506cbfdd765491
18kotov <twozerocool@gmail.com>
2017-12-24 12:46:43 +0300

    Добавлена проверка увеличения длины списка добавленных групп в тесте добавления групп.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -8,9 +8,12 @@ from model.group import Group
     
 def test_add_group(app):
     app.group.open_groups_page()
+    old_groups=app.group.get_group_list()
     app.group.create(Group(group_name="ddfg", header="4fff", footer="fbhj"))
     app.group.return_to_groups_page()
-    # app.session.logout()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+
 
 
 
